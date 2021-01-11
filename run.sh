7afdcc7d4bad01861c7d125151b1da8d9158dc39
TerMux <ter@mux.org>
2021-01-11 11:41:58 +0700

    fix
    Signed-off-by: TerMux <ter@mux.org>

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+apt install git -y
 cd /app
 bash dump.sh "https://bigota.d.miui.com/V12.0.2.0.QJWINXM/miui_CURTANAINGlobal_V12.0.2.0.QJWINXM_533770a027_10.0.zip"
+bash dump.sh "input/miui_CURTANAINGlobal_V12.0.2.0.QJWINXM_533770a027_10.0.zip"
+bash dump.sh "input/miui_CURTANAINGlobal_V12.0.2.0.QJWINXM_533770a027_10.0.zip"
+bash dump.sh "input/miui_CURTANAINGlobal_V12.0.2.0.QJWINXM_533770a027_10.0.zip"

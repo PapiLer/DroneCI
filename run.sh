18741ee1883e32ca0a5b6438fc4819cd1ae7ff12
#PapiLer <callmepapi@outlook.co.id>
2023-12-30 15:04:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/PapiLer/Project-KARA kernel -b tmp-ksu --depth=5 --recurse-submodules
 cd kernel
-curl https://raw.githubusercontent.com/Risti699/DroneCI/main/build.sh | bash
+curl https://raw.githubusercontent.com/PapiLer/DroneCI/main/build.sh | bash
